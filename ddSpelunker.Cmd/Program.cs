50145de4d44ee98cdf89ee8c9e1cb5348696d47c
Andris Engelis <andris.engelis@outlook.com>
2020-09-08 20:23:54 +0300

    simple recursive method to go trough files. Everything hardcoded

--- ddSpelunker.Cmd/Program.cs
+++ ddSpelunker.Cmd/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ddSpelunker.Cmd
 {
@@ -6,7 +8,31 @@ namespace ddSpelunker.Cmd
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("ddSpelunker started.");
+
+			var rootDirectory = @"D:\";
+
+			List<string> files = new List<string>();
+
+			files.AddRange(AllFilesInDrive(rootDirectory));
+
+			File.WriteAllLines(@"D:\content.txt", files);
+
+			Console.WriteLine("ddSpelunker ended.");
+		}
+
+		static private List<string> AllFilesInDrive(string directoryPath)
+		{
+			List<string> filesInDirectory = new List<string>();
+
+			foreach (var directory in Directory.EnumerateDirectories(directoryPath))
+			{
+				filesInDirectory.AddRange(AllFilesInDrive(directory));
+			}
+
+			filesInDirectory.AddRange(Directory.GetFiles(directoryPath));
+
+			return filesInDirectory;
 		}
 	}
 }
